25fde0b921a01c971e39dcd09b6a37349df35d5f
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-02-27 21:34:52 -0800

    updated the structure of the ui so that it is a multi tab app. The shot map inputs have been upt in as well.

--- app.R/app.R
+++ app.R/app.R
@@ -17,49 +17,34 @@ kobe <- read_csv(here("data", "data.csv")) %>%
   drop_na() %>%
   mutate(shot_made = ifelse(shot_made_flag  > 0, "scored", "missed")) #This is dropping the na values in the shots_made_flag category
 
-# Define UI for application 
-ui <- fluidPage(
-  
-  # Choosing a theme for the shiny app
-   theme = shinytheme("united"),
-   
-   # Application title
-   titlePanel("Kobe Bryant"),
-   
-   # Sidebar with a Select Input 
-   sidebarLayout(
-      sidebarPanel(
-         selectInput(inputId = "shot_type_select",
-                     label = "Choose an shot type:",
-                     choices = unique(kobe$combined_shot_type)
-           
-         )
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel("Outputs",
-         plotOutput(outputId = "shot_map")
-      )
-   )
-)
+#Defining the ui as a multi-tab app
 
-# Define server logic required to draw a histogram
+ui <- navbarPage("Kobe Bryant",
+                 tabPanel("First tab"),
+                 tabPanel("Second tab",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput(inputId = "season_select",
+                                          label = "Choose a Season",
+                                          choices = unique(kobe$season)),
+                              selectInput(inputId = "opponent_select",
+                                          label = "Choose an Opponent",
+                                          choices = unique(kobe$opponent)),
+                              selectInput(inputId = "shot_type_select",
+                                          label = "Choose a Shot Type",
+                                          choices = unique(kobe$combined_shot_type))
+                            ),
+                            mainPanel("Shot's Taken",
+                                      plotOutput(outputId = "shot_map"))
+                          )),
+                 tabPanel("Third tab"),
+                 tabPanel("Fourth tab"))
+
+
+# This is defining the server for the shiny app
 server <- function(input, output) {
   
-  #This is going to be the reactive object called shots_taken that will react to who the opponent is 
-  shots_taken <- reactive({
-    kobe %>%
-      filter == input$shot_type_select
-  })
   
-  #This is going to be the output that will render a plot of where kobe took shots from and on the court
-  output$shot_map <- renderPlot({
-    ggplot(shots_taken(),
-           aes(x = lon, y = lat)) +
-      geom_point(aes(color = shot_made))
-  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
-
