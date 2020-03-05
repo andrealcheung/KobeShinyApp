757cd610cbf78a14fc754e50d60acd483a96b818
andrealcheung <andreacheung@ucsb.edu>
2020-03-05 00:54:01 -0800

    added comments

--- Widget2.R
+++ Widget2.R
@@ -1,20 +1,24 @@
 #Attach necessary packages
 library(shiny)
 
+#######User Interface##########
 ui <- fluidPage(
   titlePanel("5 of Kobe's Greatest Moments"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("select", "Select A Moment", c("Kobe to Shaq Alley Oop","81 Points","No Flinch", "Passing Michael Jordan", "Final Game")),
+      selectInput("select", "Select A Moment", c("Kobe to Shaq Alley Oop","81 Points","No Flinch", "Passing Michael Jordan", "Final Game")), #choices
     ),
     mainPanel(
-      tabsetPanel(type = "tab",
+      tabsetPanel(type = "tab", #create different tabs
+                  tabPanel("Watch", HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/JZGEzREaYRA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')), #video embbed
                   tabPanel("What Happened", textOutput("What Happened")),
-                  tabPanel("Watch", HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/JZGEzREaYRA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')),
                   tabPanel("Official Game Recap", textOutput("recap"))
                   )
     )))
   
+
+
+########Server#########
 server <- shinyServer(function(input, output){
   output$selectmoment <- renderText(input$select)
   
