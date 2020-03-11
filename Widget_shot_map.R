e99a5e82fa5ceef57ccf665ffccc1f3f180a10eb
annapw <welterae@gmail.com>
2020-03-10 17:04:09 -0700

    text edits

--- Widget_shot_map.R
+++ Widget_shot_map.R
@@ -45,7 +45,7 @@ ui <- fluidRow(
              fluidRow(
                column(2),
                column(8,
-                      shiny::HTML("<center><h4>Kobe Bryant was an embodiment of American sports. Picked in 1996 by the Chartlotte Hornets at the age of 17, he was the first guard to ever be drafted directly out of high school. The same day he was traded to the Los Angeles Lakers, where he would remain for the next 20 years and become a symbol for Los Angeles and one of the greatest basketball players in the history of the game.")
+                      shiny::HTML("<center><h4>Kobe Bryant was an embodiment of American sports. Picked in 1996 by the Charlotte Hornets at the age of 17, he was the first guard to ever be drafted directly out of high school. The same day he was traded to the Los Angeles Lakers, where he would remain for the next 20 years and become a symbol for Los Angeles and one of the greatest basketball players in the history of the game.")
                ),
                column(2)
              ),
@@ -53,7 +53,7 @@ ui <- fluidRow(
              fluidRow(
                column(2),
                column(8,
-                      shiny::HTML("<center><h4>This project will explore 25,699 of his 30,699 field goals attempted by Kobe Bryant, from his first scoreless game against the Minnesota Timberwolves to his final 60 points game against the Utah Jazz.<h4><br>
+                      shiny::HTML("<center><h4>This project will explore 25,699 of the 30,699 field goals attempted by Kobe Bryant, from his first scoreless game against the Minnesota Timberwolves to his final 60-point game against the Utah Jazz.<h4><br>
                                   
                                   In this interactive application, you can f")
                ),
@@ -207,13 +207,7 @@ kobe2 <- read_csv("kobepoints2.csv")
     kobe_filter <- kobe2 %>%
       filter(game_score %in% (input$game_score[1]: input$game_score[2])) %>% 
       rename("Game Date"= game_date, "Opponent"= opponent, "Game Score"= game_score)
-    #input$game_score
-    # gamescore_table <- 
-    #kable(kobe_filter, col.names = c("Game Date", "Opponent", "Kobe's Total Score")) %>%
-    #kable_styling(
-    #font_size = 15,
-    # bootstrap_options = c("striped", "hover", "condensed")
-    #)
+    
   })
   
   
